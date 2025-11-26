e447c22a85fcfa0732a1335564ce90ed20c41f13
George Byte <mr.georgebyte@gmail.com>
2025-11-26 15:09:37 +0100

    Install @astrojs/mdx

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,9 @@
 import {defineConfig} from "astro/config";
+import mdx from "@astrojs/mdx";
 import {siteConfig} from "./src/config/site";
 
 // https://astro.build/config
 export default defineConfig({
     site: siteConfig.url,
+    integrations: [mdx()],
 });
