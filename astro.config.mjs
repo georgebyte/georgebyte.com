1263f0ee8458eeb2eb85f02bb75b4e88181a419c
George Byte <mr.georgebyte@gmail.com>
2025-12-08 09:27:22 +0100

    Install and configure astro-expressive-code syntax highlighter

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,18 @@
 import {defineConfig} from "astro/config";
+import astroExpressiveCode from "astro-expressive-code";
 import mdx from "@astrojs/mdx";
 import {legacyRedirects, siteConfig} from "./src/config/site";
+
+const astroExpressiveCodeOptions = {
+    themes: ["github-light"],
+    defaultProps: {
+        wrap: false,
+    },
+};
 
 // https://astro.build/config
 export default defineConfig({
     site: siteConfig.url,
-    integrations: [mdx()],
+    integrations: [astroExpressiveCode(astroExpressiveCodeOptions), mdx()],
     redirects: {...legacyRedirects},
 });
